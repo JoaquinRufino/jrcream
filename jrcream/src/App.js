30ed97f6eb93faba43c34a07e7c87d9a6f3bfd44
Joaquin Rufino <rufinojoaquin10@gmail.com>
2022-12-22 16:07:20 -0300

    "New changes"

--- jrcream/src/App.js
+++ jrcream/src/App.js
@@ -1,7 +1,7 @@
 import './components/App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import productos from './producto.json';
 import Cards from './components/Cards';
 import SelectedCard from './components/SelectedCard';
@@ -9,8 +9,10 @@ import SelectedCard from './components/SelectedCard';
 const App = () => {
   return (
     <div className="App">
-        <NavBar/>
-        <ItemListContainer/>
+        <NavBar />
+        <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        </Routes>
         <Routes>
           <Route path="/" element={<Cards />} />
           <Route path="comprar/:cardid" element={<SelectedCard productos={productos}/>} />
--- jrcream/src/components/NavBar.js
+++ jrcream/src/components/NavBar.js
@@ -1,11 +1,11 @@
 import './Navbar.css';
 import CartWidget from './CartWidget';
 
+
 const NavBar = () => {
     return <div className="Navbar-container">
         <h1 className='titulo'>Jr Cream</h1>
         <a className='button-navbar'>Inicio</a>
-        <a className='button-navbar'>Comprar</a>
         <CartWidget/>
     </div>;
 };
