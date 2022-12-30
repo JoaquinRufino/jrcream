a1bc88ea0e3499c5c66f3e08cb7c0c9cb543ab96
Joaquin Rufino <rufinojoaquin10@gmail.com>
2022-12-30 16:11:45 -0300

    "Cambios"

--- jrcream/src/components/SelectedCard.js
+++ jrcream/src/components/SelectedCard.js
@@ -1,15 +1,28 @@
 import { useParams } from "react-router-dom";
 import Card from "./Card";
+import producto from "../producto.json";
+import { useEffect, useState } from "react";
 
-
-const SelectedCard = ({productos}) => {
-    const {cardid}= useParams()
-    const selectedProduct = productos.productos.find((card)=> card.id === cardid)
-  return (
-    <div>
-        <Card productos={selectedProduct} />
-    </div>
-  )
+const SelectedCard = () => {
+const {cardid}= useParams()
+const [selectedProduct, setSelectedProduct]= useState({})
+const getOneProduct = () => {
+return new Promise ((resolve)=>{
+setTimeout(()=>{
+resolve(producto.productos.find((item)=> item.id === cardid))
+}, 100);
+})
 }
-
-export default SelectedCard;
+useEffect (()=> {
+  getOneProduct()
+  .then((res)=> setSelectedProduct(res))
+  }, [cardid]);
+  
+  return (
+  <div>
+  <Card productos={selectedProduct} />
+  </div>
+  )
+  }
+  
+  export default SelectedCard;
